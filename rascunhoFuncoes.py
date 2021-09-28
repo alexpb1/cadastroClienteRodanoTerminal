e854d3f1d5bfec1f2235c3b9d34083e3ecc419ab
alexpb1 <alexsandropb3@gmail.com>
2021-09-28 06:34:41 -0300

    busca melhorada

--- rascunhoFuncoes.py
+++ rascunhoFuncoes.py
@@ -1,10 +1,12 @@
 import sqlite3
 import os
 lista=[]
+encontrado=[]
 n1=0
 n2=0
 i=j=100
 opcao=''
+contador=0
 
 def cabecario():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -27,8 +29,10 @@ def menu():
         cabecario()
         cadastrar_cliente()
         menu()
-    if opcao not in ('1','2','3','4'):
-     menu()
+    if opcao =='3':
+        menu()
+    if opcao =='4':
+        menu()
     
 
 def cadastrar_cliente():
@@ -51,7 +55,7 @@ def cadastrar_cliente():
 
 
 def buscar_cliente(objeto):
-    global n1, n2, i, j, lista
+    global n1, n2,lista,encontrado
     caminho="D:\\DB\\dbTeste.db"
     conexao=sqlite3.connect(caminho) 
     c=conexao.cursor()
@@ -62,13 +66,11 @@ def buscar_cliente(objeto):
         linha=list(linha1)
         del linha[0]
         lista.append(linha)
-
     if objeto!="":
         for line in lista:  
             for coluna in line:
                 if coluna==objeto:
-                    i=n2
-                    j=n1
+                    encontrado.append(lista[n2])
                 if n1<len(lista[0]):
                     n1+=1
                 if n1==len(lista[0]):
@@ -77,21 +79,34 @@ def buscar_cliente(objeto):
         n2=0
     else:
         buscar_cliente()
-
-    if i!=100 and j!=100:
-        print('\033[31m','\n Nome: ', lista[i][0])
-        print('\n Endereço: ', lista[i][1])
-        print('\n Telefone: ', lista[i][2])
-        print('\n Email: ', lista[i][3])
+    if len(encontrado)==1:
+        print('\033[31m','\n Nome: ', encontrado[0][0])
+        print('\n Endereço: ', encontrado[0][1])
+        print('\n Telefone: ', encontrado[0][2])
+        print('\n Email: ', encontrado[0][3])
         print('\033[34m')
         input('\nTecle enter para retornar ao menu... \n')
-    else:
+    if len(encontrado)>1:
+        contador=1
+        print('\nEncontramos os seguintes clientes: \n')
+        for pessoa in encontrado:
+            print (contador, ' - ',pessoa)
+            contador+=1
+        cliente_select=int(input('\nDigite o número referente a um cliente: \n'))-1
+        cabecario()
+        print('\033[31m','\n Nome: ', encontrado[cliente_select][0])
+        print('\n Endereço: ', encontrado[cliente_select][1])
+        print('\n Telefone: ', encontrado[cliente_select][2])
+        print('\n Email: ', encontrado[cliente_select][3])
+        print('\033[34m')
+        input('\nTecle enter para retornar ao menu... \n')
+    if len(encontrado) ==0:
         print('\033[31m','\n Nenhum cliente localizado.')
         print('\033[34m')
         input('\nTecle enter para retornar ao menu... \n')
-    i=j=100
     conexao.close()
     lista.clear()
+    encontrado.clear()
 
 
 
