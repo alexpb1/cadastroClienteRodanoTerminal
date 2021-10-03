88da0c26e9e990d6f592ec985cbcceb6c6eb05ee
alexpb1 <alexsandropb3@gmail.com>
2021-10-03 08:35:59 -0300

    finalizado versao beta1

--- CADASTRO_CLIENTE.py
+++ CADASTRO_CLIENTE.py
@@ -9,19 +9,34 @@ contador=0
 cliente_selecionado=0
 cliente_select=0
 
+def conexaoDB(sql):
+    caminho = caminho=os.getcwd()+ "\CLIENTES_DB.db"
+    con=None
+    print(caminho)
+    try:
+        con=sqlite3.connect(caminho)
+        c=con.cursor()
+        c.execute(sql)
+        con.commit()
+        con.close()
+    except:
+        print("Falha na conexão com DB")
+
 def cabecario():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls')
     print('\033[34m','\n=============== CADASTRO DE CLIENTES ==================\n')
+    print('\033[31m')
 
 def menu():
     global opcao
     opcao=0
     cabecario()
-    print('\n 1 - Buscar Cliente')
+    print(' 1 - Buscar Cliente')
     print('\n 2 - Cadastrar Cliente')
     print('\n 3 - Editar Cliente')
-    print('\n 4 - Deletar Cliente \n')
-    print('\n')
+    print('\n 4 - Deletar Cliente')
+    print('\n 5 - Sair\n')
+    print('\033[34m')
     opcao=input("Digite a opção desejada: ")
 
     if opcao=='1':
@@ -40,7 +55,8 @@ def menu():
         exibir_busca()
         if cliente_selecionado!=-1:
             escolha=input('Quer mesmo editar? ')
-            if escolha =='s':
+            if escolha.upper()=='S':
+                cabecario()
                 editar_cliente()
         menu()
     if opcao =='4':
@@ -49,35 +65,37 @@ def menu():
         exibir_busca()
         if cliente_selecionado!=-1:
             escolha=input('Digite S para deletar? ')
-            if escolha =='s':
+            if escolha.upper()=='S':
                 deletar_cliente()
+        menu()
+    if opcao=='5':
+        os.system('cls')
+        print('PROGRAMA ENCERRADO!')
+        quit()
+    else:
         menu()
 
 def cadastrar_cliente():
-    global caminho, c
+    global caminho
     nome=input("Nome do cliente: ")
+    print('\n')
     endereco=input("Endereço: ")
+    print('\n')
     fone=input("Telefone: ")
+    print('\n')
     email=input("Endereço de email: ")
     print('\n')
     confirmacao=input('Digite S para gravar o cliente. ')
     if confirmacao.upper()=='S':
-        caminho="D:\\DB\\dbTeste.db"
-        conexao=sqlite3.connect(caminho)
-        c=conexao.cursor()
-        c.execute("""
-        INSERT INTO tb_clientes (NOME, ENDERECO, FONE, EMAIL)
-        VALUES (?, ?, ?, ?)
-        """,(nome, endereco, fone, email))
-        conexao.commit()
-        conexao.close()
+        vsql="INSERT INTO tb_clientes (NOME, ENDERECO, FONE, EMAIL) VALUES('"+nome+"', '"+endereco+"','"+fone+"','"+email+"')"
+        conexaoDB(vsql)
         print('\033[34m',)
 
 def buscar_cliente():
     global n1, n2,lista,encontrado
     lista.clear()
     encontrado.clear()
-    caminho="D:\\DB\\dbTeste.db"
+    caminho=os.getcwd()+ "\CLIENTES_DB.db"
     conexao=sqlite3.connect(caminho) 
     c=conexao.cursor()
     c.execute("""
@@ -120,30 +138,25 @@ def exibir_busca():
         if cliente_select >= 0 and cliente_select<len(encontrado):
             cliente_selecionado=encontrado[cliente_select][0]
             cabecario()
-            print('\033[31m','\n Nome: ', encontrado[cliente_select][1])
+            print('\033[31m','Nome: ', encontrado[cliente_select][1])
             print('\n Endereço: ', encontrado[cliente_select][2])
             print('\n Telefone: ', encontrado[cliente_select][3])
             print('\n Email: ', encontrado[cliente_select][4])
             print('\033[34m')
         
     if len(encontrado) ==0:
-        print('\033[31m','\n Nenhum cliente localizado.')
+        print('\033[31m','\n NENHUM CLIENTE LOCALIZADO.')
         print('\033[34m')
 
 def deletar_cliente():
     global cliente_selecionado
-    print('função de deletar sendo executado')
-    caminho="D:\\DB\\dbTeste.db"
-    conexao=sqlite3.connect(caminho)
-    c=conexao.cursor()
-    c.execute("DELETE FROM tb_clientes WHERE N_ID_CLIENTE=?", (cliente_selecionado,))
-    conexao.commit()
-    conexao.close()
+    str_cliente_selecionado=str(cliente_selecionado)
+    vsql="DELETE FROM tb_clientes WHERE N_ID_CLIENTE='"+str_cliente_selecionado+"'"
+    conexaoDB(vsql)
 
 def editar_cliente():
     global cliente_selecionado, cliente_select
-    cabecario()
-    print('\nNome atual: ', encontrado[cliente_select][1])
+    print('Nome atual: ', encontrado[cliente_select][1])
     NovoNome=input('Novo nome: ')
     print('\nEndereço atual: ', encontrado[cliente_select][2])
     NovoEndereco=input('Novo Endereço: ')
@@ -153,12 +166,13 @@ def editar_cliente():
     NovoEmail=input('Novo Email: ')
     print('\n')
     confirmacao=input('Digite S para gravar as mudanças no cliente. ')
-    caminho="D:\\DB\\dbTeste.db"
-    conexao=sqlite3.connect(caminho)
-    c=conexao.cursor()
-    c.execute("UPDATE tb_clientes SET nome =?, endereco=?, fone=?, email=? WHERE N_ID_CLIENTE = ?",(NovoNome, NovoEndereco, NovoTelefone, NovoEmail, cliente_selecionado))
-    conexao.commit()
-    conexao.close()
+    if confirmacao.upper()=='S':
+        caminho=os.getcwd()+ "\CLIENTES_DB.db"
+        conexao=sqlite3.connect(caminho)
+        c=conexao.cursor()
+        c.execute("UPDATE tb_clientes SET nome =?, endereco=?, fone=?, email=? WHERE N_ID_CLIENTE = ?",(NovoNome, NovoEndereco, NovoTelefone, NovoEmail, cliente_selecionado))
+        conexao.commit()
+        conexao.close()
 
 cabecario()
 menu()
--- teste.py
+++ teste.py
@@ -1,12 +1,75 @@
-import sqlite3
-cliente_a_deletar=input('digite número do cliente à deletar. ')
+import os
 
-def deletar_cliente():
-    caminho="D:\\DB\\dbTeste.db"
-    conexao=sqlite3.connect(caminho)
-    c=conexao.cursor()
-    c.execute("DELETE FROM tb_clientes WHERE N_ID_CLIENTE=?", (cliente_a_deletar,))
-    conexao.commit()
-    conexao.close()
-  
-deletar_cliente()
+taxa=0.04
+tempo=3
+C=20000
+divisao=8
+
+Saldo_Devedor1=C*(1+taxa)**tempo
+
+J1=Saldo_Devedor1-C
+
+parcela1=Saldo_Devedor1/8
+Saldo_devedor2=Saldo_Devedor1-parcela1
+os.system('cls')
+
+print('Saldo devedor 1: ',round(Saldo_Devedor1,2))
+print('Parcela: ',round(parcela1, 2),'     Juros: ', round((J1/8), 2))
+print('Saldo devedor 2: ',round(Saldo_devedor2,2))
+print('=============================================')
+
+Saldo_devedor3=(Saldo_devedor2-parcela1)*(1+taxa)
+parcela2=Saldo_devedor2/7
+
+print('Saldo devedor 2: ',round(Saldo_devedor2,2))
+print('Parcela: ',round(parcela2, 2),'     Juros: ', round((J1/7), 2))
+print('Saldo devedor 3: ',round(Saldo_devedor3,2))
+print('=============================================')
+
+Saldo_devedor4=(Saldo_devedor3-parcela2)*(1+taxa)
+parcela3=Saldo_devedor3/6
+
+print('Saldo devedor 3: ',round(Saldo_devedor3,2))
+print('Parcela: ',round(parcela3, 2),'     Juros: ', round((J1/6), 2))
+print('Saldo devedor 4: ',round(Saldo_devedor4,2))
+print('=============================================')
+
+Saldo_devedor5=(Saldo_devedor4-parcela3)*(1+taxa)
+parcela4=Saldo_devedor4/5
+
+print('Saldo devedor 4: ',round(Saldo_devedor4,2))
+print('Parcela: ',round(parcela4, 2),'     Juros: ', round((J1/5), 2))
+print('Saldo devedor 5: ',round(Saldo_devedor5,2))
+print('=============================================')
+
+Saldo_devedor6=(Saldo_devedor5-parcela4)*(1+taxa)
+parcela5=Saldo_devedor5/4
+
+print('Saldo devedor 5: ',round(Saldo_devedor5,2))
+print('Parcela: ',round(parcela5, 2),'     Juros: ', round((J1/4), 2))
+print('Saldo devedor 6: ',round(Saldo_devedor6,2))
+print('=============================================')
+
+Saldo_devedor7=(Saldo_devedor6-parcela5)*(1+taxa)
+parcela6=Saldo_devedor6/3
+
+print('Saldo devedor 6: ',round(Saldo_devedor6,2))
+print('Parcela: ',round(parcela6, 2),'     Juros: ', round((J1/3), 2))
+print('Saldo devedor 7: ',round(Saldo_devedor7,2))
+print('=============================================')
+
+Saldo_devedor8=(Saldo_devedor7-parcela5)*(1+taxa)
+parcela7=Saldo_devedor7/2
+
+print('Saldo devedor 7: ',round(Saldo_devedor7,2))
+print('Parcela: ',round(parcela7, 2),'     Juros: ', round((J1/2), 2))
+print('Saldo devedor 8: ',round(Saldo_devedor8,2))
+print('=============================================')
+
+Saldo_devedor9=(Saldo_devedor8-parcela6)*(1+taxa)
+parcela8=Saldo_devedor8/1
+
+print('Saldo devedor 8: ',round(Saldo_devedor8,2))
+print('Parcela: ',round(parcela8, 2),'     Juros: ', round((J1/1), 2))
+print('Saldo devedor 9: ',round(0,2))
+print('=============================================')
